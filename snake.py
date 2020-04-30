a8f79cab0df9f8d093c38176c29a9632126838cf
dirtylittledirtbike <cwestnedge@gmail.com>
2020-04-29 19:03:54 -0500

    boundaries

--- snake.py
+++ snake.py
@@ -4,8 +4,8 @@ import locale
 
 locale.setlocale(locale.LC_ALL, '')
 
-message1 = '       _(\_/)\n      ((((^.\ \n    ((((  (6 \ \n   ((((( .    \ \n (((((  / ._  ,`, \n((((   /    `-.- \n((((   / '
-message2 = '               ,%%%, \n             ,%%%` % \n            ,%%`( `| \n           ,%%@ /\_/ \n ,%.-"""--%%% "@@__ \n %%/   ' \
+horse1 = '       _(\_/)\n      ((((^.\ \n    ((((  (6 \ \n   ((((( .    \ \n (((((  / ._  ,`, \n((((   /    `-.- \n((((   / '
+horse2 = '               ,%%%, \n             ,%%%` % \n            ,%%`( `| \n           ,%%@ /\_/ \n ,%.-"""--%%% "@@__ \n %%/   ' \
     '          |__`\ \n.%`\     |   \   /  // \n,%` >   .`----\ |  [/ \n  < <<`       || \n    `\\\       || ' \
         '\n      )\\      )\ \n '
 
@@ -15,8 +15,9 @@ sh, sw = s.getmaxyx()
 w = curses.newwin(sh, sw, 0, 0)
 w.keypad(1)
 w.timeout(100)
+#w.border()
 
-snk_x = int(sw/4)
+snk_x = int(sw/2)
 snk_y = int(sh/2)
 snake = [
     [snk_y, snk_x],
@@ -24,10 +25,18 @@ snake = [
     [snk_y, snk_x-2]
 ]
 
-food = [sh/2, sw/2]
-w.addstr(0, 0, message2)
-w.addstr(sh - int(sh/4) - 2, 0, message1)
+food = [sh/2 + 10, sw/2]
+w.addstr(1, 0, horse2)
+w.addstr(sh - int(sh/4) - 2, 0, horse1)
 w.addstr(int(food[0]), int(food[1]), '@')
+
+for i in range(0, sh):
+    for j in range(0, sw):
+        w.addstr(i, int(sw/3),'|')
+        #        w.addstr(0, j, '-')
+        #        w.addstr(int(sw/3.5),i, '|')
+#        w.addstr(i, sw-2, '|')
+
 
 key = curses.KEY_RIGHT
 counter = 0
@@ -45,9 +54,12 @@ my_dict = {'0': sent1, '1':sent2, '2': sent3, '3': \
 dict_vals = 0
 dict_keys = 0
 reverse = False
+score = 0
 
 while True:
-    
+    # display score
+    w.addstr(0, 0, 'Score: ' + str(score))
+   
     if dict_vals < len(my_dict[str(dict_keys)]) - 1:
         dict_vals += 1
     else:
@@ -56,7 +68,7 @@ while True:
     next_key = w.getch()
     key = key if next_key == -1 else next_key
 
-    if snake[0][0] in [0, sh] or snake[0][1]  in [0, sw] or snake[0] in snake[1:]:
+    if snake[0][0] in [0, sh] or snake[0][1]  in [int(sw/3) + 1, sw] or snake[0] in snake[1:]:
         curses.endwin()
         quit()
 
@@ -83,11 +95,12 @@ while True:
 
         dict_vals = 0
         food = None
+        score += 1
         
         while food is None:
             nf = [
                 random.randint(1, sh-1),
-                random.randint(int(sw/4) + 3, sw-1)
+                random.randint(int(sw/3) + 1, sw-1)
             ]
             food = nf if nf not in snake else None
 #        w.addch(food[0], food[1], curses.ACS_DIAMOND)
